1be97ae45396de735ec834a92746ab69fb702ea8
CindyvdVries <cindyvdvries@gmail.com>
2024-08-13 11:29:41 +0200

    refactor

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <navigation-bar />
+    <v-main>
+      <map-component />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import NavigationBar from '@/components/NavigationBar'
+import MapComponent from '@/components/MapComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavigationBar,
+    MapComponent
+  }
+}
+</script>
---
+++ frontend/src/components/MapComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="map">
+      <mapbox-map
+        ref="mapboxmap"
+        class="map"
+        :access-token="mapboxToken"
+        map-style="mapbox://styles/mapbox/standard"
+        :preserveDrawingBuffer="true"
+      >
+      <MapboxNavigationControl :visualizePitch="true" />
+    </mapbox-map>
+    </div>
+</template>
+
+<script>
+// import mapboxgl from 'mapbox-gl'
+import { MapboxMap, MapboxNavigationControl } from '@studiometa/vue-mapbox-gl'
+
+export default {
+  data () {
+    return {
+      mapboxToken: process.env.VUE_APP_MAPBOX_TOKEN
+    }
+  },
+  components: {
+    MapboxMap,
+    MapboxNavigationControl
+  },
+  mounted () {
+    console.log(process.env.VUE_APP_MAPBOX_TOKEN)
+  }
+}
+
+</script>
+
+<style lang="css">
+.mapboxgl-map, .map {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
