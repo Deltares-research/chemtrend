ae46ac22b3c55a9ea5cf90b54687b33f7bfec5a6
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2024-08-13 16:36:19 +0200

    autocomplete and list for future categories

--- frontend/src/components/NavigationDrawerTabs.vue
+++ frontend/src/components/NavigationDrawerTabs.vue
@@ -1,19 +1,44 @@
 <template>
   <div>
-    <v-tabs v-model="tab" >
-      <v-tab value="substances"> <v-icon> mdi-flask </v-icon> </v-tab>
-      <v-tab value="layers"> <v-icon> mdi-layers </v-icon> </v-tab>
-      <v-tab value="trends"> <v-icon> mdi-chart-line </v-icon> </v-tab>
-      <v-tab value="information"> <v-icon> mdi-information </v-icon> </v-tab>
+    <v-tabs v-model="tab">
+      <v-tab value="substances">
+        <v-icon>mdi-flask</v-icon>
+      </v-tab>
+      <v-tab value="layers">
+        <v-icon>mdi-layers</v-icon>
+      </v-tab>
+      <v-tab value="trends">
+        <v-icon>mdi-chart-line</v-icon>
+      </v-tab>
+      <v-tab value="information">
+        <v-icon>mdi-information</v-icon>
+      </v-tab>
     </v-tabs>
 
     <v-card-text>
       <v-tabs-window v-model="tab">
-        <v-tabs-window-item value="substances">
-          <v-select
-            label="Select substance"
-            :items="substanceList"
-          ></v-select>
+        <v-tabs-window-item value="substances" class="tab-content">
+          <div class="content-wrapper">
+            <v-col>
+              <v-row>
+                <v-autocomplete
+                  label="Select substance"
+                  :items="substanceList"
+                  class="autocomplete-list"
+                ></v-autocomplete>
+              </v-row>
+              <v-row>
+                <v-expansion-panels flat>
+                  <v-expansion-panel>
+                    <v-expansion-panel-title>All substances</v-expansion-panel-title>
+                    <v-expansion-panel-text>
+                      <v-list :items="substanceList"></v-list>
+                    </v-expansion-panel-text>
+                  </v-expansion-panel>
+                </v-expansion-panels>
+              </v-row>
+            </v-col>
+          </div>
         </v-tabs-window-item>
 
         <v-tabs-window-item value="layers">
@@ -27,7 +52,6 @@
         <v-tabs-window-item value="information">
           Information
         </v-tabs-window-item>
-
       </v-tabs-window>
     </v-card-text>
   </div>
@@ -45,3 +69,19 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.tab-content {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.content-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+}
+</style>
