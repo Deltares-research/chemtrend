b5bba0609f555e98067d154c21ec1e5d7ae00443
Wouter <wouter.schoonveld@deltares.nl>
2024-08-13 16:28:16 +0200

    connect with db and end point substances

---
+++ backend/chemtrend/models.py
@@ -0,0 +1,9 @@
+from typing import Any, Dict, List, Optional, Type
+from pydantic import BaseModel
+
+
+class Substance(BaseModel):
+    substance_id: int
+    substance_code: str
+    substance_description: str
+    cas: str
