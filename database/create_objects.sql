3239c02eef57dc3bdb27189bad12308dd689d65f
Wouter <wouter.schoonveld@deltares.nl>
2024-08-14 12:00:46 +0200

    CHEMTREND-3: show only available substances from example data

--- database/create_objects.sql
+++ database/create_objects.sql
@@ -1,12 +1,13 @@
 create schema if not exists chemtrend;
 
 -- substances
-create or replace view chemtrend.substance as
-select parameter_id as substance_id
-, parameter_code as substance_code
-, parameter_omschrijving as substance_description
-, "CAS"
-from public.parameter;
+create or replace view chemtrend.substance(substance_id, substance_code, substance_description, cas) as
+SELECT p.parameter_id           AS substance_id,
+       p.parameter_code         AS substance_code,
+       p.parameter_omschrijving AS substance_description,
+       p."CAS" as cas
+FROM parameter p join (select distinct parameter_code from voorbeelddata.trend) t on t.parameter_code=p.parameter_code
+;
 
 -- locations: FOR NOW: BASED ON EXAMPLE DATA
 create or replace view chemtrend.location as
