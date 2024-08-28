916fd991220e93054bbdb7629a9ca58cd6949672
Wouter <wouter.schoonveld@deltares.nl>
2024-08-28 14:54:43 +0200

    add view chemtrend.location_substance

--- database/create_objects.sql
+++ database/create_objects.sql
@@ -1,3 +1,4 @@
+-- drop schema if exists chemtrend cascade;
 create schema if not exists chemtrend;
 
 -- substances
@@ -19,6 +20,22 @@ create or replace view chemtrend.location_geojson as
 -- select distinct meetpunt_code as location_code, st_asgeojson(geom) as geom from voorbeelddata.trend;
 select json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(location)::json)) as geojson from chemtrend.location;
 
+-- view with locations and its trend color per parameter
+create or replace view chemtrend.location_substance as
+select location_code, parameter_code, color, geom
+from (
+    select meetpunt_code as location_code
+    , parameter_code
+    , geom
+    , case skendall_trend
+        when 'trend opwaarts' then 'red'
+        when 'geen trend' then 'grey'
+        when 'trend neerwaarts' then 'green'
+    end as color
+    from voorbeelddata.trend tr
+) x
+group by location_code, parameter_code, color, geom
+;
 
 -- grant access to all users
 GRANT ALL ON all tables in schema chemtrend TO vries_cy;
