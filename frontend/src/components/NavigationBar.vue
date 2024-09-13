ba26b2ecc193eeaa0cb95974b5cfd9143f326853
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2024-09-13 16:55:04 +0200

    logos

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -5,5 +5,21 @@
     </template>
 
     <v-app-bar-title>CHEMtrend</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <img :src=rwsImgPath alt="Rijkswaterstaat Logo" height="40" />
+    <img :src=deltaresImgPath alt="Deltares Logo" height="40" />
   </v-app-bar>
 </template>
+
+<script>
+import deltaresLogo from '../assets/Deltares_logo_D-blauw_RGB.png'
+import rwsLogo from '../assets/Rijkswaterstaat.png'
+export default {
+  data () {
+    return {
+      deltaresImgPath: deltaresLogo,
+      rwsImgPath: rwsLogo
+    }
+  }
+}
+</script>
