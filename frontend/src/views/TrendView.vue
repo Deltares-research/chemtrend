6f5c0362e1835015c59b05a070b40644903f70dd
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2024-10-14 16:03:06 +0200

    hardcode expansible cards

--- frontend/src/views/TrendView.vue
+++ frontend/src/views/TrendView.vue
@@ -1,8 +1,31 @@
 <template>
   <div class="trend-view">
-    <div v-for="trend in trends" :key="trend.name" class="trend-wrapper">
-      <v-card :title="trend.name" class="trend">
-        <v-chart class="chart" :option="trend.option" autoresize />
+    <div v-for="(trend, index) in trends" :key="trend.name" class="trend-wrapper">
+      <v-card v-if="expandedTrends[index]">
+        <v-card class="expansible-card">
+          <v-btn @click="toggleExpand(index, 0)">
+          {{ expandedTrends[index][0] ? 'Collapse' : 'Expand' }}
+        </v-btn>
+        <v-expand-transition>
+          <div v-if="expandedTrends[index][0]">
+            <v-card :title="trend.name" class="trend">
+              <v-chart class="chart" :option="trend.option" autoresize />
+            </v-card>
+          </div>
+        </v-expand-transition>
+        </v-card>
+        <v-card class="expansible-card">
+          <v-btn @click="toggleExpand(index, 1)">
+          {{ expandedTrends[index][1] ? 'Collapse' : 'Expand' }}
+        </v-btn>
+        <v-expand-transition>
+          <div v-if="expandedTrends[index][1]">
+            <v-card :title="trend.name" class="trend">
+              <v-chart class="chart" :option="trend.option" autoresize />
+            </v-card>
+          </div>
+        </v-expand-transition>
+        </v-card>
       </v-card>
     </div>
   </div>
@@ -35,6 +58,22 @@ export default {
   },
   data () {
     return {
+      expandedTrends: []
+    }
+  },
+  watch: {
+    trends: {
+      handler (newTrends) {
+        this.expandedTrends = newTrends.map(() => [true, true])
+      },
+      immediate: true // Run the watcher immediately on first render
+    }
+  },
+  methods: {
+    toggleExpand (trendIndex, cardIndex) {
+      if (this.expandedTrends[trendIndex]) {
+        this.expandedTrends[trendIndex][cardIndex] = !this.expandedTrends[trendIndex][cardIndex]
+      }
     }
   }
 }
@@ -53,10 +92,16 @@ export default {
   display: flex;
   justify-content: center;
   margin: 10px;
+  height: fit-content;
 }
 
 .trend {
   height: 45vh;
   width: 70vw;
 }
+
+.expansible-card {
+  width: 70vw;
+}
+
 </style>
