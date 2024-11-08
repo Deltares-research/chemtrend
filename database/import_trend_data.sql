55462f455475d493456a9f408b21ce4428f6b3ab
Wouter <wouter.schoonveld@deltares.nl>
2024-11-08 16:56:59 +0100

    import trend data: trend_regio

---
+++ database/import_trend_data.sql
@@ -0,0 +1,22 @@
+--  trend_regio
+insert into public.trend_regio (regio_id, parameter_id, eenheid_id, hoedanigheid_id, compartiment_id, datum, lowess_p0, lowess_p25, lowess_p75)
+select
+r.regio_id,
+p.parameter_id,
+e.eenheid_id,
+h.hoedanigheid_id,
+c.compartiment_id,
+datum::date,
+lowess_p0,
+lowess_p25,
+lowess_p75
+-- select count(*)      -- 188843
+from import."05_data_trend_ats_waterbeheerder" imp
+join public.regio r on r.regio_type_id=4 and r.bron_id=imp.waterbeheerder_code::int --125968; rijkswateren vallen weg
+join public.parameter p on p.parameter_code=imp.parameter_code
+join public.eenheid e on e.eenheid_code=imp.eenheid_code
+join public.hoedanigheid h on h.hoedanigheid_code=imp.hoedanigheid_code
+join public.compartiment c on c.compartiment_code=imp.compartiment_code
+;
+
+-- TO DO: trend_locatie
