f9b3d83a22b60383c619e798266bbb49f69ef888
Wouter <wouter.schoonveld@deltares.nl>
2024-11-10 10:52:03 +0100

    importscript tbv trend data per locatie

--- database/import_trend_data.sql
+++ database/import_trend_data.sql
@@ -19,4 +19,32 @@ join public.hoedanigheid h on h.hoedanigheid_code=imp.hoedanigheid_code
 join public.compartiment c on c.compartiment_code=imp.compartiment_code
 ;
 
--- TO DO: trend_locatie
+-- trend_locatie
+insert into public.trend_locatie(meetpunt_id, parameter_id, eenheid_id, hoedanigheid_id, compartiment_id, kwaliteitsoordeel_id, datum, tijd, waarde_meting, ats_y, lowline_y, skendall_trend)
+select
+l.meetpunt_id meetpunt_id,
+p.parameter_id,
+e.eenheid_id,
+h.hoedanigheid_id,
+c.compartiment_id,
+k.kwaliteitsoordeel_id,
+imp.datum::date,
+imp.tijd::time,
+imp.waarden::numeric as waarde_meting,
+imp.ats_y::numeric,
+imp.lowline_y::numeric,
+case imp.skendall_trend
+    when 'trend neerwaarts' then -1
+    when 'geen trend' then 0
+    when 'trend opwaarts' then 1
+    end ::smallint as skendall_trend
+-- select count(*) --885279
+from import."03_data_trend_ats_info" imp
+join public.locatie l on l.meetpunt_code_2022=imp.meetpunt_code_2022
+join public.parameter p on p.parameter_code=imp.parameter_code
+join public.eenheid e on e.eenheid_code=imp.eenheid_code
+join public.hoedanigheid h on h.hoedanigheid_code=imp.hoedanigheid_code
+join public.compartiment c on c.compartiment_code=imp.compartiment_code
+join public.kwaliteitsoordeel k on k.kwaliteitsoordeel_code=coalesce(imp.kwaliteitsoordeel_code,'00')
+;
+
