4577ca1b5a72eca9677c1742d4d5074da3bd4aef
Wouter <wouter.schoonveld@deltares.nl>
2024-11-26 13:23:59 +0100

    remove outdated views on regional levels

--- database/create_objects.sql
+++ database/create_objects.sql
@@ -1,63 +1,6 @@
 drop schema if exists chemtrend cascade;
 create schema if not exists chemtrend;
 
--- provinces
-create or replace view chemtrend.province(province_id, province_code, province_description, geometry) as
-SELECT pr."FID"           AS province_id,
-       pr."Code"         AS province_code,
-       pr."Provincien" AS province_description,
-       st_transform(pr.geometry, 28992) as geometry
-FROM public.provincies pr;
-
-create or replace view chemtrend.province_geojson as
-select json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(province)::json)) as geojson
-from chemtrend.province;
-
--- waterboards
-create or replace view chemtrend.waterboard(waterboard_id, waterboard_code, waterboard_description, geometry) as
-SELECT wb.waterbeheerder_id          AS waterboard_id,
-       wb.waterbeheerder_code         AS waterboard_code,
-       wb.waterbeheerder_omschrijving AS waterboard_description,
-       wb.geometry as geometry
-FROM public.waterbeheerder wb
-where st_isempty(geometry)=false;
-;
-
-create or replace view chemtrend.waterboard_geojson as
-select json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(waterboard)::json)) as geojson
-from chemtrend.waterboard;
-
-
--- sub catchments
-create or replace view chemtrend.catchment(catchment_id, catchment_code, catchment_description_short, catchment_description_long, geometry) as
-SELECT dsg."OBJECTID"          AS catchment_id,
-       dsg."GAFIDENT"         AS catchment_code,
-       dsg."GAFNAAM" AS catchment_description_short,
-	   dsg."NAAM" AS catchment_description_long,
-       dsg.geometry as geometry
-FROM public.deelstroomgebieden dsg;
-
-create or replace view chemtrend.catchment_geojson as
-select json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(catchment)::json)) as geojson
-from chemtrend.catchment;
-
-
--- KRW-waterbodies
-create or replace view chemtrend.waterbody(KRWwaterbody_id, KRWwaterbody_code, KRWwaterbody_description, KRWwaterbody_namespace, catchment_code, KRWwaterbody_status, KRWwaterbody_type_code, geometry) as
-SELECT wl.db_id_extern          AS "waterbody_id",
-       wl.waterlichaam_code         AS "waterbody_code",
-       wl.waterlichaam_omschrijving AS "waterbody_description",
-	   wl."namespace" AS "waterbody_namespace",
-	   wl.stroomgebied_code AS catchment_code,
-	   wl.waterlichaam_status AS "waterbody_status",
-	   wl."waterlichaam_KRWtype_code" AS "waterbody_type_code",
-       wl.geometry as geometry
-FROM public."KRW_waterlichaam" wl;
-
-create or replace view chemtrend.waterbody_geojson as
-select json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(waterbody)::json)) as geojson
-from chemtrend.waterbody;
-
 -- substances
 create or replace view chemtrend.substance(substance_id, substance_code, substance_description, cas) as
 SELECT p.parameter_id           AS substance_id,
@@ -167,13 +110,6 @@ from (
 -- view with all regions
 drop view if exists chemtrend.region cascade;
 create or replace view chemtrend.region as
--- select province_id+1000 as region_id, 'Provincie'::varchar as region_type, province_id as original_id, province_description as region_description, geometry from chemtrend.province
--- union all
--- select waterboard_id+2000 as region_id, 'Waterschap'::varchar as region_type, waterboard_id as original_id, waterboard_description as region_description, geometry from chemtrend.waterboard
--- union all
--- select krwwaterbody_id+3000 as region_id, 'Waterlichaam'::varchar as region_type, krwwaterbody_id as original_id, krwwaterbody_description as region_description, geometry from chemtrend.waterbody
--- union all
--- select catchment_id+4000 as region_id, 'Stroomgebied'::varchar as region_type, catchment_id as original_id, catchment_description_short as region_description, geometry from chemtrend.catchment
 select r.regio_id as region_id, rt.regio_type as region_type, r.bron_id as original_id, r.regio_omschrijving as region_description, geom, geom_rd
 from public.regio r
 join public.regio_type rt on rt.regio_type_id=r.regio_type_id
