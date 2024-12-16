cbee257fac05e4ce08335aa2ab15fb61e6c53fbe
loudavid <marielou.david@deltares.nl>
2024-12-16 10:48:08 +0100

    removing useless modification

--- frontend/src/components/MapComponent.vue
+++ frontend/src/components/MapComponent.vue
@@ -281,8 +281,6 @@ export default {
         if (substanceId) {
           this.addTrend({ x, y, substanceId, name, currentLocation: location })
           this.$emit('update:bottomPanel', true)
-        } else {
-          this.selectedSubstance = false // does not work
         }
       })
     }
