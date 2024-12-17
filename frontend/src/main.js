acb0970b3624bb3ef982a4e0b8d5891eb85df8bf
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2024-12-17 18:51:05 +0100

    packages and fix vue-tour Vue3 incompatibility

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,15 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import { loadFonts } from './plugins/webfontloader'
+import VueTour from './plugins/vue-tour'
 
 loadFonts()
 
-createApp(App)
-  .use(router)
-  .use(store)
-  .use(vuetify)
-  .mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+app.use(vuetify)
+app.use(VueTour)
+
+app.mount('#app')
