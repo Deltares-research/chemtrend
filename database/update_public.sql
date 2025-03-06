f3f24b8723c06a266aec20a4f9bcc6518b475ef1
Wouter Schoonveld <35798126+wschoonveld@users.noreply.github.com>
2025-03-06 11:25:31 +0100

    CHEMTREND-114: rename columns (for new data)

--- database/update_public.sql
+++ database/update_public.sql
@@ -1,12 +1,12 @@
 -- regio type (tbv tabel regio)
-drop table if exists public.regio_type;
+drop table if exists public.regio_type cascade;
 create table if not exists public.regio_type(
     regio_type_id int primary key,
     regio_type varchar
 );
 
 -- regio: verzameling van regionale polygonen van verschillende niveaus (provincie, waterschap, etc.) in één tabel
-drop table if exists public.regio;
+drop table if exists public.regio cascade;
 create table if not exists public.regio (
     regio_id serial primary key,
     bron_id int,
@@ -24,9 +24,9 @@ insert into public.regio_type (regio_type_id, regio_type) VALUES (1,'Nederland')
 insert into public.regio (bron_id, regio_type_id, regio_omschrijving, geom, geom_rd)
 select 0 as bron_id, 1 as regio_type_id, 'Nederland' as regio_omschrijving, st_transform(geom_rd, 4326) as geom, geom_rd
 from (
-    select st_setsrid(st_extent(geom),28992) as geom_rd
+    select st_setsrid(st_extent(geometry),28992) as geom_rd
     from public.locatie
-    where st_isempty(geom)=false
+    where st_isempty(geometry)=false
 ) q;
 
 insert into public.regio (bron_id, regio_type_id, regio_omschrijving, geom, geom_rd)
@@ -53,23 +53,34 @@ where st_isempty(geometry)=false
 --------- LOCATIES KOPPELEN AAN REGIO -----------
 create schema if not exists temp;
 -- 1. koppeltabel aanmaken
+drop table if exists public.locatie_regio cascade;
 create table public.locatie_regio (
     locatie_regio_id serial primary key,
     meetpunt_id int references public.locatie(meetpunt_id),
     regio_id int references public.regio(regio_id)
 );
+-- view to use for regional data
+drop view if exists public.locatie_regio_info;
+create or replace view public.locatie_regio_info as
+select r.regio_id, r.regio_omschrijving, rt.regio_type, l.meetpunt_id, l.meetpunt_code_2023 as meetpunt_code, rt.regio_type_id
+from public.regio r
+join public.regio_type rt on r.regio_type_id = rt.regio_type_id
+join public.locatie_regio lr on r.regio_id = lr.regio_id
+join public.locatie l on lr.meetpunt_id = l.meetpunt_id
+;
+
 -- 2. locatiekoppeling o.b.v. geometrie
 insert into public.locatie_regio (meetpunt_id, regio_id)
 select l.meetpunt_id, r.regio_id
 from public.locatie l
-join public.regio r on 1=1 and st_within(l.geom, r.geom_rd) and st_isempty(l.geom)=false
+join public.regio r on 1=1 and st_within(l.geometry, r.geom_rd) and st_isempty(l.geometry)=false
 --     uitgezonderd regio NL (want aparte query)
     and r.regio_type_id>1
 ;
 -- 3. correctie: meetpunten op grensgebied of onder beheer van RWS:
 drop table if exists temp.correctie_locatie_regio;
 -- (vul temp tabel)
-select l.meetpunt_id, l.meetpunt_code_2022, w.waterbeheerder_id, w.waterbeheerder_code, w.waterbeheerder_omschrijving, r.regio_id, r.bron_id, r.regio_omschrijving
+select l.meetpunt_id, l.meetpunt_code_2023, w.waterbeheerder_id, w.waterbeheerder_code, w.waterbeheerder_omschrijving, r.regio_id, r.bron_id, r.regio_omschrijving
 , wr.regio_id as new_regio_id
 into temp.correctie_locatie_regio
 from public.locatie l
@@ -91,7 +102,7 @@ left join public.locatie_regio_info lr on lr.meetpunt_id=l.meetpunt_id and lr.re
 join public.waterbeheerder w on l.waterbeheerder_id = w.waterbeheerder_id
 join public.regio r2b on r2b.regio_type_id=4 and r2b.bron_id=l.waterbeheerder_id
 where lr.meetpunt_id is null
-and st_isempty(l.geom)=false
+and st_isempty(l.geometry)=false
 ;
 insert into public.locatie_regio (meetpunt_id, regio_id)
 select meetpunt_id, regio_id from temp.aanvulling_loc_reg;
@@ -100,17 +111,8 @@ select meetpunt_id, regio_id from temp.aanvulling_loc_reg;
 insert into public.locatie_regio (meetpunt_id, regio_id)
 select l.meetpunt_id, r.regio_id
 from public.locatie l join public.regio r on 1=1 and r.regio_type_id=1
-where st_isempty(l.geom)=false;
+where st_isempty(l.geometry)=false;
 
--- view to use for regional data
-drop view public.locatie_regio_info;
-create or replace view public.locatie_regio_info as
-select r.regio_id, r.regio_omschrijving, rt.regio_type, l.meetpunt_id, l.meetpunt_code_2022 as meetpunt_code, rt.regio_type_id
-from public.regio r
-join public.regio_type rt on r.regio_type_id = rt.regio_type_id
-join public.locatie_regio lr on r.regio_id = lr.regio_id
-join public.locatie l on lr.meetpunt_id = l.meetpunt_id
-;
 
 -- trend data
 create table public.trend_regio (
@@ -123,7 +125,8 @@ create table public.trend_regio (
     datum date,
     lowess_p25 numeric,
     lowess_p50 numeric,--staat in importtabel als p0
-    lowess_p75 numeric
+    lowess_p75 numeric,
+    trend_period int
 );
 
 -- drop table public.trend_locatie cascade;
@@ -144,11 +147,12 @@ create table public.trend_locatie (
     p_value_trend numeric,
     theilsen_slope numeric,
     rapportagegrens bool,
-    ats_slope numeric
+    ats_slope numeric,
+    trend_period int
 );
 
 -- add index to locatie table
-create index ix_locatie3 on public.locatie(meetpunt_id, meetpunt_code_2022) include (geom);
+create index ix_locatie3 on public.locatie(meetpunt_id, meetpunt_code_2023) include (geometry);
 
 -- extra indexes:
 create index if not exists ix_locatie_regio on public.locatie_regio(meetpunt_id, regio_id);
