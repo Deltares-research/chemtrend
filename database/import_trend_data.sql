ea75b1a9b340aa3e87de982282b8db0ec4b6bff4
Wouter Schoonveld <35798126+wschoonveld@users.noreply.github.com>
2025-03-13 11:40:45 +0100

    trend_locatie bijgewerkt incl trendperiode

--- database/import_trend_data.sql
+++ database/import_trend_data.sql
@@ -1,6 +1,6 @@
--- trend_locatie
+-- trend_locatie: empty tabel
 truncate table public.trend_locatie;
-insert into public.trend_locatie(meetpunt_id, parameter_id, eenheid_id, hoedanigheid_id, compartiment_id, kwaliteitsoordeel_id, datum, tijd, waarde_meting, ats_y, lowline_y, trend_conclusie, p_value_trend, theilsen_slope, rapportagegrens, ats_slope)
+insert into public.trend_locatie(meetpunt_id, parameter_id, eenheid_id, hoedanigheid_id, compartiment_id, kwaliteitsoordeel_id, datum, tijd, waarde_meting, ats_y, lowline_y, trend_conclusie, p_value_trend, theilsen_slope, rapportagegrens, ats_slope, trend_period)
 select
 l.meetpunt_id meetpunt_id,
 p.parameter_id,
@@ -21,11 +21,18 @@ case imp.trend_conclusie
 imp.p_value_trend::numeric,
 imp.theilsen_slope::numeric,
 imp.rg::bool as rapportagegrens,
-imp.ats_slope::numeric as ats_slope
--- TO DO: trend_period toevoegen
--- select count(*) --885279
-from import."03_data_trend_ats_info" imp
-join public.locatie l on l.meetpunt_code_2022=imp.meetpunt_code_2022
+imp.ats_slope::numeric as ats_slope,
+imp.trend_period
+from (
+    -- select count(*) --1206053    --> 1202886 (=excl recs met meetpunt_code_2023=NULL)
+    select *, 0::int as trend_period from import."03_data_trend_ats_info"
+    union all
+    -- select count(*) --969857 --> 966941
+    select *, 1::int as trend_period from import."03B_data_trend_ats_info_vanaf_2009"
+    -- TIJDELIJKE FIX: data <2009 weglaten
+    WHERE datum>='2009-01-01'
+)imp
+join public.locatie l on l.meetpunt_code_2023=imp.meetpunt_code_2023
 join public.parameter p on p.parameter_code=imp.parameter_code
 join public.eenheid e on e.eenheid_code=imp.eenheid_code
 join public.hoedanigheid h on h.hoedanigheid_code=imp.hoedanigheid_code
@@ -33,9 +40,11 @@ join public.compartiment c on c.compartiment_code=imp.compartiment_code
 join public.kwaliteitsoordeel k on k.kwaliteitsoordeel_code=coalesce(imp.kwaliteitsoordeel_code,'00')
 ;
 
+
+
 --  trend_regio: waterbeheerder
 truncate table public.trend_regio;
-insert into public.trend_regio (regio_id, parameter_id, eenheid_id, hoedanigheid_id, compartiment_id, datum, lowess_p0, lowess_p25, lowess_p75)
+insert into public.trend_regio (regio_id, parameter_id, eenheid_id, hoedanigheid_id, compartiment_id, datum, lowess_p0, lowess_p25, lowess_p75, trend_period)
 select
 r.regio_id,
 p.parameter_id,
@@ -45,10 +54,11 @@ c.compartiment_id,
 datum::date,
 lowess_p0,
 lowess_p25,
-lowess_p75
+lowess_p75,
+, NULL trend_period
 -- TO DO: trend_period toevoegen
 -- select count(*)      -- 188843
-from import."05_data_trend_ats_waterbeheerder" imp
+from import.05_data_trend_ats_waterbeheerder imp
 join public.regio r on r.regio_type_id=4 and r.bron_id=imp.waterbeheerder_code::int --125968; rijkswateren vallen weg
 join public.parameter p on p.parameter_code=imp.parameter_code
 join public.eenheid e on e.eenheid_code=imp.eenheid_code
@@ -57,7 +67,7 @@ join public.compartiment c on c.compartiment_code=imp.compartiment_code
 ;
 
 --  trend_regio: overig (provincie, waterlichaam, stroomgebied, landelijk)
-insert into public.trend_regio (regio_id, parameter_id, eenheid_id, hoedanigheid_id, compartiment_id, datum, lowess_p0, lowess_p25, lowess_p75)
+insert into public.trend_regio (regio_id, parameter_id, eenheid_id, hoedanigheid_id, compartiment_id, datum, lowess_p0, lowess_p25, lowess_p75, trend_period)
 select
 imp.regio_id,
 p.parameter_id,
@@ -67,7 +77,8 @@ c.compartiment_id,
 datum::date,
 lowess_p0,
 lowess_p25,
-lowess_p75
+lowess_p75,
+,null as trend_period
 -- TO DO: trend_period toevoegen
 from (
     select * from import."06_data_trend_ats_stroomgebied"
